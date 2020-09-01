cbe6b0cb03d51d9ce257fa5b2c8e5d894c832b37
Sachin Pandey <sach0499@gmail.com>
2020-09-01 14:05:20 +0530

    added domElements

---
+++ js/domElements.js
@@ -0,0 +1,13 @@
+export const domElements = {
+
+     prevArrow: document.querySelector('.prev'),
+     nextArrow: document.querySelector('.next'),
+     monthTitle: document.querySelector('.month-top'),
+
+     addNoteBtn: document.querySelector('.event-add'),
+     editNoteBtn: document.querySelector('.event-edit'),
+     deleteNoteBtn: document.querySelector('.event-delete'),
+
+
+}
+
